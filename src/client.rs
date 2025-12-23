6aeaac3da6fe53ff465b676913971c48432169f0
Fanda Vacek <fvacek@elektroline.cz>
2025-12-23 23:19:16 +0100

    Fix SHV API ver discovery, make V3 to be fallback
    Node app was present also in shvbroker V3 running in SHV compat mode.
    Node clients is unique for SHV2 broker and does not exist in SHV2
    compatibility mode.

--- src/client.rs
+++ src/client.rs
@@ -273,13 +273,14 @@ impl<V: ClientVariant> Client<V> {
                 .await?
                 .iter()
                 .find_map(|node| match node.as_str() {
-                    "app" => Some(ShvApiVersion::V2),
-                    "client" => Some(ShvApiVersion::V3),
-                    _ => None,
-                })
+                        "client" => Some(ShvApiVersion::V3),
+                        "clients" => Some(ShvApiVersion::V2),
+                        _ => None,
+                    }
+                )
             .unwrap_or_else(|| {
-                warn!("Cannot detect SHV API version. Using version 2 as a fallback.");
-                ShvApiVersion::V2
+                warn!("Cannot detect SHV API version. Using version 3 as a fallback.");
+                ShvApiVersion::V3
             });
             Ok(api_version)
         }
