762a7c4be21585723f969e8e9ae823df999e60f2
Jaroslav Beran <jara.beran@gmail.com>
2026-04-27 18:08:19 +0200

    Fix malformed bugfix of heartbeat timer reset

--- src/connection.rs
+++ src/connection.rs
@@ -235,13 +235,13 @@ async fn connection_loop(
                                 // Reset heartbeat timer only for request frames, that is where we can expect a response.
                                 if message.is_request() {
                                     fut_heartbeat_timeout = futures_time::task::sleep(heartbeat_interval.into()).fuse();
-                                    if let Err(err) = writer_tx.unbounded_send(message) {
-                                        warn!("Cannot send message to the writer task: {err}");
-                                        conn_event_sender
-                                            .unbounded_send(ConnectionEvent::Disconnected)
-                                            .unwrap_or_else(|e| debug!("ConnectionEvent::Disconnected send failed: {e}"));
-                                        return ConnectionLoopResult::ConnectionClosed;
-                                    }
+                                }
+                                if let Err(err) = writer_tx.unbounded_send(message) {
+                                    warn!("Cannot send message to the writer task: {err}");
+                                    conn_event_sender
+                                        .unbounded_send(ConnectionEvent::Disconnected)
+                                        .unwrap_or_else(|e| debug!("ConnectionEvent::Disconnected send failed: {e}"));
+                                    return ConnectionLoopResult::ConnectionClosed;
                                 }
                             },
                         }
